5df59ea562791535ecb5d479e384c55d60cb6d18
Leonidas <jesus.leon1@unmsm.edu.pe>
2022-07-29 00:50:58 -0500

    feat: add alert

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SideBar from './components/sideBar/SideBar';
 import Home from './pages/home/Home';
 import ToDo from './pages/toDo/ToDo';
 
-import "./app.css";
+import "./App.css";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
--- src/components/createTask/CreateTask.jsx
+++ src/components/createTask/CreateTask.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
 import "./createTask.css"
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 export default function CreateTask({ todo, setTodo, todos, setTodos }) {
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleClick = () => {
+      setOpen(true);
+    };
+
+    const handleClose = (event, reason) => {
+      if (reason === "clickaway") {
+        return;
+      }
+
+      setOpen(false);
+    };
+
     function handleSubmit(e){
         e.preventDefault();
 
@@ -12,8 +35,21 @@ export default function CreateTask({ todo, setTodo, todos, setTodos }) {
         };
 
         if(!newTodo.text){
-          alert("Incomplete data");
-          return;
+           return (
+             <Snackbar
+               open={open}
+               autoHideDuration={6000}
+               onClose={handleClose}
+             >
+               <Alert
+                 onClose={handleClose}
+                 severity="warning"
+                 sx={{ width: "100%" }}
+               >
+                 This is a success message!
+               </Alert>
+             </Snackbar>
+           );
         }
 
         setTodos([...todos].concat(newTodo));
@@ -22,17 +58,17 @@ export default function CreateTask({ todo, setTodo, todos, setTodos }) {
 
   return (
     <>
-        <h1 className="title">Todo List</h1>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            onChange={(e) => setTodo(e.target.value)}
-            value={todo}
-          />
-          <button className="buttonAdd" type="submit">
-            Add Todo
-          </button>
-        </form>
+      <h1 className="title">Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+        />
+        <button onClick={handleClick} className="buttonAdd" type="submit">
+          Add Todo
+        </button>
+      </form>
     </>
-  )
+  );
 }
